08e9455cc13e1ae15266bc10ebfc428dd5c38836
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-26 16:45:34 -0400

    Agregando EntradasDetalle al Models

---
+++ Prueba_AP1_P2/Shared/Models/EntredasDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_AP1_P2.Shared.Models
+{
+    public class EntredasDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public int ProductoId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "El campo esta fuera de rango")]
+        public double CantidadUtilizada { get; set; }
+    }
+}
