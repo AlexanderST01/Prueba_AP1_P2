97ff5cb2a3b139095e5b01c3f2efbb7ecb7ed884
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-26 16:50:39 -0400

    Arreglando en Productos que ProductoId sea foranea en otro modelo

--- Prueba_AP1_P2/Shared/Models/Productos.cs
+++ Prueba_AP1_P2/Shared/Models/Productos.cs
@@ -15,7 +15,7 @@ namespace Prueba_AP1_P2.Shared.Models
         [Required(ErrorMessage = "El campo es obligatorio")]
         public string Descripcion { get; set; }
         public int Tipo { get; set; }
-        [ForeignKey("EntradasId")]
+        [ForeignKey("ProductosId")]
         public ICollection<EntredasDetalle> EntredasDetalles { get; set; } = new List<EntredasDetalle>();
     }
 }
