c81ed1ae2fb2e3b0ac0dc2ec52cf5724b5738d71
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-11-01 01:22:46 -0400

    Aumentando Exitencia en OnModelCreating

--- Prueba_AP1_P2/Server/DAL/Context.cs
+++ Prueba_AP1_P2/Server/DAL/Context.cs
@@ -14,10 +14,10 @@ namespace Prueba_AP1_P2.Server.DAL
         {
             modelBuilder.Entity<Productos>().HasData(new List<Productos>()
             {
-                new Productos() { ProductoId = 1, Descripcion = "Maní", Existencia = 40, Tipo = 0 },
-                new Productos() { ProductoId = 2, Descripcion = "Pistachos", Existencia = 600, Tipo = 0 },
-                new Productos() { ProductoId = 3, Descripcion = "Pasas", Existencia = 500, Tipo = 0 },
-                new Productos() { ProductoId = 4, Descripcion = "Ciruelas", Existencia = 700, Tipo = 0 },
+                new Productos() { ProductoId = 1, Descripcion = "Maní", Existencia = 40000, Tipo = 0 },
+                new Productos() { ProductoId = 2, Descripcion = "Pistachos", Existencia = 60000, Tipo = 0 },
+                new Productos() { ProductoId = 3, Descripcion = "Pasas", Existencia = 50000, Tipo = 0 },
+                new Productos() { ProductoId = 4, Descripcion = "Ciruelas", Existencia = 70000, Tipo = 0 },
                 new Productos() { ProductoId = 5, Descripcion = "Mixto MPP 0.5lb", Existencia = 0, Tipo = 1 },
                 new Productos() { ProductoId = 6, Descripcion = "Mixto MPC 0.5lb", Existencia = 0, Tipo = 1 },
                 new Productos() { ProductoId = 7, Descripcion = "Mixto MPP 0.2lb", Existencia = 0, Tipo = 1 }
